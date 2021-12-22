9c555dbaa27b0708650d0dde18097db3d1ced833
Swastik Singh <singhswastik29@gmail.com>
2021-12-22 13:18:12 +0530

    - Added necessary packages for setting up an Express backend server - Added mongoose package for establishing a MongoDB database connection - Using dotenv for process environment variables

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+
+dotenv.config();
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB for Social Media Connected..."))
+  .catch((err) => console.log(err));
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server Running at ${port}`);
+});
